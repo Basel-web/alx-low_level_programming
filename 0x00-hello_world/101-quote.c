2d897529e5bd15f2526dc26f3f5b362b0797cb0f
Basel Ahmed <zain.ahmedioq@gmail.com>
2023-02-18 01:37:12 -0800

    unistd.h liberary

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<intfd>
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",58);
+	return (1);
+}
