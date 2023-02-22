0629308d545844a47356a7e5151bc729c0a5733b
Basel Ahmed <zain.ahmedioq@gmail.com>
2023-02-21 17:02:10 -0800

    d

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ *  * times_table - hehe
+ *   * Description: main fun
+ *    * Return: 0
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
