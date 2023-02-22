c83f291028aae1f77d772304405ce6564f2a1015
Basel Ahmed <zain.ahmedioq@gmail.com>
2023-02-21 17:39:14 -0800

    f

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * * * _abs - main fun
+ * * Description: main fun
+ * * @n: int n
+ * * Return: 0
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+		return (-n);
+	return (n);
+}
