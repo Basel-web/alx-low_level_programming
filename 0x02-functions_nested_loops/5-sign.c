7df3e6be842827a7073f796cfc883c31ca743bed
Basel Ahmed <zain.ahmedioq@gmail.com>
2023-02-21 17:10:46 -0800

    f

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ *  * _isalpha - main func
+ *   * Description: heho
+ *    * @c: heho
+ *     * Return: 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		return (1);
+	else if (n < 0)
+		return (-1);
+	else
+		return (0);
+}
