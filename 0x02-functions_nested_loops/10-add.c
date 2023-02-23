dce33ecaca9a68e46a645798be67fd5bb4a0f72b
Basel Ahmed <zain.ahmedioq@gmail.com>
2023-02-22 18:29:31 -0800

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,13 +2,10 @@
 #include"main.h"
 
 /**
- *  * @brief Adds two integers
- *   *
- *    * This function takes two integers as input and returns their sum.
- *     *
- *      * @param a The first integer to be added
- *       * @param b The second integer to be added
- *        * @return The sum of the two integers
+ *add  This function takes two integers as input and returns their sum.
+ * @param a The first integer to be added
+ * @param b The second integer to be added
+ *  @return The sum of the two integers
  */
 
 int add(int a, int b)
