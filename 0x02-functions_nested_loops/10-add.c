8afdd607101fdb8ae926d46907b9953f0187e26e
Basel Ahmed <zain.ahmedioq@gmail.com>
2023-02-22 18:27:41 -0800

    add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,9 +2,13 @@
 #include"main.h"
 
 /**
- *  add - prints the sum of a and b
- * @a
- * @b
+ *  * @brief Adds two integers
+ *   *
+ *    * This function takes two integers as input and returns their sum.
+ *     *
+ *      * @param a The first integer to be added
+ *       * @param b The second integer to be added
+ *        * @return The sum of the two integers
  */
 
 int add(int a, int b)
