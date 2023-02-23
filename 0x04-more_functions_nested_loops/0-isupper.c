1a6a6205004f302b66c4e0268386bc7a0df73de7
Basel Ahmed <zain.ahmedioq@gmail.com>
2023-02-23 04:46:19 -0800

    ha

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,17 @@
 #include<stdio.h>
 #include"main.h"
 
-/*int _isupper-check ig the input is an uppercase letter
- * @n:parameter
+/**
+ *int _isupper-starting with 1 and 2,
+ *@n:rated by a comma followed by a space.
+ *@:Return: Always 0.
  */
+
 
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
 		return (1);
-	else 
+	else
 		return (0);
 }
