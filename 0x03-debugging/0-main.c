6e9954c6c626789fd7ba656f425199e8de8bddfe
Basel Ahmed <zain.ahmedioq@gmail.com>
2023-02-22 20:40:37 -0800

    !

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -4,13 +4,13 @@
 /**
  *  * main - tests the positive_or_negative function with a case of 0
  *   * Return: Always 0
- *    */
+ */
 int main(void)
 {
-	    int zero = 0;
+        int zero = 0;
 
-	        positive_or_negative(zero);
+        positive_or_negative(zero);
 
-		    return (0);
+	return (0);
 }
 
