e4f0c610b9565cc597ee5b596d311d31f705e3e3
Basel Ahmed <zain.ahmedioq@gmail.com>
2023-02-22 18:41:28 -0800

    d

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include"main.h"
+/**
+ * print_to_98 - prints n to 98
+ * @n var
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i < 99; i++)
+	{
+		_putchar(i);
+		if (i != 98)
+		{
+		_putchar(',');
+		_putchar(' ');
+		}
+	}
+}
